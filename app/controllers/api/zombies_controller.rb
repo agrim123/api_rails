79470d1a283015957574f353a0a7520e31bd92f3
agrim mittal <agrimmittal97@gmail.com>
2016-12-05 00:59:15 +0530

    url for creating zombies

--- app/controllers/api/zombies_controller.rb
+++ app/controllers/api/zombies_controller.rb
@@ -5,14 +5,27 @@ module API
 			if weapon = params[:weapon]
 				zombies = zombies.where(weapon: weapon)
 			end
-			render json: zombies, status: 200  #or status: ok
+			 render json: zombies, status: 200 
+=begin
+			respond_to do |format|	
+				format.json { render json: zombies, status: 200 }
+				format.xml { render xml: zombies, status: 200 }
+			end 
+=end 
 		end
 		def create
-			zombie = Zombie.create
+			zombie = Zombie.new(zombie_params)
+			if zombie.save
+				render json: zombie,status: 201 #,location: zombie
+			end
 		end
 		def show	
 			zombie = Zombie.find(params[:id])	
 			render json: zombie, status: 200	
 		end	
+		private
+		def zombie_params
+			params.require(:zombie).permit(:name,:weapon)
+		end
 	end
 end
--- test/integration/listing_zombies_test.rb
+++ test/integration/listing_zombies_test.rb
@@ -4,7 +4,7 @@ class ListingZombiesTest < ActionDispatch::IntegrationTest
 	setup { host! 'api.zombies.com' }	
 
 	test 'returns zombies filtered by weapon' do	
-		get '/zombies?weapon=axe'	
+		get '/zombies.json?weapon=axe'	
 		assert_equal 200, response.status	
 		zombies = JSON.parse(response.body, symbolize_names: true)
 		names = zombies.collect { |z| z[:name] }	
@@ -16,4 +16,9 @@ class ListingZombiesTest < ActionDispatch::IntegrationTest
 		assert_equal 200, response.status	
 		assert_equal Mime[:json], response.content_type
 	end
+	test 'returns zombies in XML' do	
+		get '/zombies', {}, { 'Accept' => Mime[:xml] }
+		assert_equal 200, response.status	
+		assert_equal Mime[:xml], response.content_type
+	end	
 end
