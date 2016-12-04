a53426d04d0e64c01d605193d6506a54e965980b
agrim mittal <agrimmittal97@gmail.com>
2016-12-04 23:05:16 +0530

    generated controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	constraints subdomain: 'api' do
+		namespace :api, path '/' do
+			resources :zombies
+		end
+	end
+	resources :pages
 end
